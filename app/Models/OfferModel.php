ecdfa043a721ff3f1cca3d6fa80e295f65fe65f6
webaufbau <info@webaufbau.ch>
2025-06-25 19:37:11 +0200

    integrated my admin

--- app/Models/OfferModel.php
+++ app/Models/OfferModel.php
@@ -45,6 +45,7 @@ class OfferModel extends Model
     protected function beforeInsert(array $data): array
     {
         $fields = json_decode($data['data']['form_fields'] ?? '{}', true);
+        $userInputs = $fields['__submission']['user_inputs'] ?? [];
 
         if (is_array($fields)) {
             $data['data']['type'] = $this->detectType($fields);
@@ -52,10 +53,10 @@ class OfferModel extends Model
             $data['data']['zip'] = $fields['auszug_adresse_firma']['zip'] ?? null;
             $data['data']['customer_type'] = isset($fields['firmenname']) ? 'firma' : 'privat';
 
-            $data['data']['firstname'] = $fields['vorname'] ?? null;
-            $data['data']['lastname'] = $fields['nachname'] ?? null;
-            $data['data']['email'] = $fields['email'] ?? null;
-            $data['data']['phone'] = $fields['phone'] ?? null;
+            $data['data']['firstname'] = $fields['vorname'] ?? $userInputs['vorname'] ?? null;
+            $data['data']['lastname'] = $fields['nachname'] ?? $userInputs['nachname'] ?? null;
+            $data['data']['email'] = $fields['email'] ?? $userInputs['email'] ?? null;
+            $data['data']['phone'] = $fields['phone'] ?? $userInputs['phone'] ?? null;
             $data['data']['additional_service'] = $fields['additional_service'] ?? null;
             $data['data']['service_url'] = $fields['service_url'] ?? null;
             $data['data']['uuid'] = $data['data']['uuid'] ?? bin2hex(random_bytes(16));
@@ -68,7 +69,7 @@ class OfferModel extends Model
     {
         $source = $fields['service_url'] ?? '';
 
-        if (str_contains($source, 'umzug')) return 'move';
+        if (str_contains($source, 'umzuege')) return 'move';
         if (str_contains($source, 'reinigung')) return 'cleaning';
         if (str_contains($source, 'maler')) return 'painter';
         if (str_contains($source, 'garten')) return 'gardener';
