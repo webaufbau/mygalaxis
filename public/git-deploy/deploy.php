95d7d3844f451fe0d7ba4988285559aeb49e4ec8
webaufbau <info@webaufbau.ch>
2025-06-27 15:23:50 +0200

    deploy

--- public/git-deploy/deploy.php
+++ public/git-deploy/deploy.php
@@ -3,7 +3,7 @@ include_once('/home/famajynu/www/my_offertenschweiz_ch/git_deploy_token.php');
 
 define("REMOTE_REPOSITORY", "git@github.com:webaufbau/mygalaxis.git"); // The SSH URL to your repository
 define("DIR", "/home/famajynu/www/my_offertenschweiz_ch/");                          // The path to your repostiroy; this must begin with a forward slash (/)
-define("BRANCH", "refs/heads/main");                                 // The branch route
+define("BRANCH", "main");                                 // The branch route
 define("LOGFILE", "/home/famajynu/my_offertenschweiz_ch_deploy.log");                                       // The name of the file you want to log to.
 define("GIT", "/usr/bin/git");                                         // The path to the git executable
 define("MAX_EXECUTION_TIME", 180);                                     // Override for PHP's max_execution_time (may need set in php.ini)
