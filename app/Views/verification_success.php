f3282efac78631cfb505ebb6415e3c0c9d14866f
webaufbau <info@webaufbau.ch>
2025-06-27 22:10:12 +0200

    deploy

--- app/Views/verification_success.php
+++ app/Views/verification_success.php
@@ -4,8 +4,31 @@
     <meta charset="UTF-8">
     <title>Verifizierung erfolgreich</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="1;url=<?=$next_url;?>" />
+
+    <style>
+        .elementor-1384 .elementor-element.elementor-element-67308e5:not(.elementor-motion-effects-element-type-background), .elementor-1384 .elementor-element.elementor-element-67308e5 > .elementor-motion-effects-container > .elementor-motion-effects-layer {
+            background-color: #955CE9;
+        }
+        .elementor-widget-container {
+            text-align: center;
+        }
+    </style>
 </head>
 <body class="bg-light">
+
+<div data-elementor-type="header" data-elementor-id="1384" class="elementor elementor-1384 elementor-location-header" data-elementor-post-type="elementor_library">
+    <div class="elementor-element elementor-element-67308e5 e-con-full e-flex e-con e-parent e-lazyloaded" data-id="67308e5" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+        <div class="elementor-element elementor-element-817a056 elementor-widget elementor-widget-image" data-id="817a056" data-element_type="widget" data-widget_type="image.default">
+            <div class="elementor-widget-container">
+                <a href="https://offertenschweiz.ch/">
+                    <img src="https://offertenschweiz.ch/wp-content/uploads/2025/06/OFFERTENSchweiz00001.ch_.png" class="attachment-large size-large wp-image-1581" alt="offertenschweiz-logo" srcset="https://offertenschweiz.ch/wp-content/uploads/2025/06/OFFERTENSchweiz00001.ch_.png 1005w, https://offertenschweiz.ch/wp-content/uploads/2025/06/OFFERTENSchweiz00001.ch_-300x31.png 300w, https://offertenschweiz.ch/wp-content/uploads/2025/06/OFFERTENSchweiz00001.ch_-768x79.png 768w" sizes="(max-width: 800px) 100vw, 800px">								</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="container mt-5">
     <div class="alert alert-success">
         ✅ Deine Telefonnummer wurde erfolgreich verifiziert!
