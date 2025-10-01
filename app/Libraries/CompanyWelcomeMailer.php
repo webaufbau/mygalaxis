317f1537cfd80de1fa20a7a8eed306e4af9fe2fa
webaufbau <info@webaufbau.ch>
2025-10-01 18:19:01 +0200

    company register new mail

---
+++ app/Libraries/CompanyWelcomeMailer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Libraries;
+
+use App\Entities\User;
+
+class CompanyWelcomeMailer
+{
+    public function sendWelcomeEmail(User $user): bool
+    {
+        $siteConfig = siteconfig();
+
+        $subject = "Danke für Ihre Anmeldung als Firma";
+        $message = view('emails/company_welcome', [
+            'user' => $user,
+            'company_name' => $user->company_name,
+            'company_email' => $user->company_email,
+            'siteConfig' => $siteConfig,
+            'website_name' => $siteConfig->name,
+            'website_email' => $siteConfig->email,
+            'backend_url' => $siteConfig->backendUrl,
+        ]);
+
+        $view = \Config\Services::renderer();
+        $fullEmail = $view->setData([
+            'title' => 'Willkommen bei ' . $siteConfig->name,
+            'content' => $message,
+        ])->render('emails/layout');
+
+        $email = \Config\Services::email();
+        $email->setTo($user->getEmail());
+        $email->setFrom($siteConfig->email, $siteConfig->name);
+        $email->setSubject($subject);
+        $email->setMessage($fullEmail);
+        $email->setMailType('html');
+
+        date_default_timezone_set('Europe/Zurich');
+        $email->setHeader('Date', date('r'));
+
+        if (!$email->send()) {
+            log_message('error', 'Willkommens-Mail konnte nicht gesendet werden: ' . print_r($email->printDebugger(), true));
+            return false;
+        }
+
+        return true;
+    }
+}
